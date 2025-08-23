a9a362365d80497916eb14bc9e41a39e15976a35
sumzali <alisummer066@icloud.com>
2025-08-23 23:02:54 +0100

    created dictionary to store task

---
+++ todo.py
@@ -0,0 +1,26 @@
+
+#Step 2: Decide How to Store Tasks
+#Use a list of dictionaries in Python.
+#Each dictionary will represent a single task with at least 2 keys:
+#'"task" → the text of the task.
+#"done" → a boolean (True/False).
+#Example in your head:
+#[
+ # {"task": "Buy groceries", "done": False},
+  #{"task": "Finish homework", "done": True}
+#]
+
+myTasks = {
+  "task1" : {
+    "task" : "send Email",
+    "done" : True
+  },
+  "task2" : {
+    "task" : "Buy groceries",
+    "done" : False
+  },
+  "task3" : {
+    "task" : "Finish homework",
+    "done" : True
+  }
+}
